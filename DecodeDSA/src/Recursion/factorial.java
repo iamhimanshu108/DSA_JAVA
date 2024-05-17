5021a3b40f3838bff1f2e38651046ed7ef65afc8
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2024-05-17 16:20:44 +0530

    MAY 17

---
+++ DecodeDSA/src/Recursion/factorial.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class factorial {
+    public static  int fact(int n ){
+        if(n==1) return 1;
+        int ans = n * fact(n-1);
+
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n : ");
+        int n = sc.nextInt();
+        System.out.println(fact(n));
+    }
+}
