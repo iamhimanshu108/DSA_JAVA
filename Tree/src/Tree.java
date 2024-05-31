bedefcf420ac09a352ba64e03b8af7ac80f5b41d
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2024-05-31 20:14:00 +0530

    MAY 31

---
+++ Tree/src/Tree.java
@@ -0,0 +1,14 @@
+public class Tree {
+
+
+    int data;
+    Tree left;
+    Tree right;
+
+    Tree(int data)
+    {
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+}
