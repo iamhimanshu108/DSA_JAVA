8134f31caa28a12ab6e81851e69a3743c4b40af6
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2024-06-08 20:51:57 +0530

    JUNE 8

---
+++ DecodeDSA/src/LinkedList/ListNodeClass.java
@@ -0,0 +1,16 @@
+package LinkedList;
+class Car{
+    int price;
+    String name;
+    Car(int price, String name){
+
+    }
+
+}
+public class ListNodeClass {
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        c1.name = "Kia Sonnet";
+        c1.price = 120000;
+    }
+}
