16ebebbe2870bcb25e73fb58518fce01fb4c116f
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2024-07-25 09:17:06 +0530

    JULY 25

---
+++ DecodeDSA/src/String/whyStrings.java
@@ -0,0 +1,14 @@
+package String;
+
+import java.sql.SQLOutput;
+
+public class whyStrings {
+    public static void main(String[] args) {
+        // "Himanshu"
+        char[] arr = {'H','I','M','K'};
+
+        for(int ele : arr){
+            System.out.print(ele);
+        }
+    }
+}
